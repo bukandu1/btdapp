01716b22ef4b6384efbe72d7627f3aa9e33abfa2
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-01-16 19:38:01 -0500

    Added config documentation to connect to SQLite database.

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
-from  flask import (Flask,  render_template)
+from flask import render_template, Flask
 
-app  = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def home():
+    """
+    Returns  homepage template
+    """
+
+    return  render_template("home.html")
+
+
+# Run if main file
+if __name__ == "__main__":
+    app.run(debug=True)
