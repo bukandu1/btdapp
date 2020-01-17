2823cd813c5f5bb08abcbf0aeb69c8cd07e51bbb
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-01-16 19:57:09 -0500

    Initialize empty files for bucket list and yaml file.

--- server.py
+++ server.py
@@ -1,6 +1,15 @@
 from flask import render_template, Flask
+import config
 
 app = Flask(__name__, template_folder="templates")
+
+# Get application instance
+connex_app = config.connex_app
+
+# Read YAML file to configure endpoints
+connex_app.add_api("swagger.yaml")
+
+config.init_db()
 
 @app.route("/")
 def home():
@@ -10,6 +19,10 @@ def home():
 
     return  render_template("home.html")
 
+
+@app.route("/ api/bucketList")
+def bucketList():
+    pass
 
 # Run if main file
 if __name__ == "__main__":
